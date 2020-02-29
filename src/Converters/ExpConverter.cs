4f872078a078e7f5024a7ae4ff354a323726d652
obrunorf <obrunorf@gmail.com>
2020-02-28 20:42:40 -0500

    xpppp

---
+++ src/Converters/ExpConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using Pokedex.Abstractions;
+
+namespace Pokedex.Pokerole.Converters
+{
+    public class ExpConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string)
+            {   if (value.Equals(".")) { return "??? "; }
+            else
+                return $"{value} xp";
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
