701362235e4c037f51e1b3b0faee420b2443a2e6
obrunorf <obrunorf@gmail.com>
2020-08-02 14:48:15 -0400

    aaa

---
+++ src/Converters/NumberConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Pokedex.Pokerole.Converters
+{
+    public class NumberConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int)
+            {
+                return $"#{value.ToString()} - ";
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
